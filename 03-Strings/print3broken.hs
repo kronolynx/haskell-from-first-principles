c942e9ce75e1591387544b8029765159f0d380b0
Johann Og <kronolynx@gmail.com>
2019-12-10 13:47:40 +0100

    Because pigs can't fly

--- 03-Strings/print3broken.hs
+++ 03-Strings/print3broken.hs
@@ -3,6 +3,7 @@ module Print3Broken where
 main :: IO()
 main = do
   putStrLn greeting
+
   printSecond
   where greeting = "Yarrrr"
         printSecond = do putStrLn greeting
---
+++ 04-Basic-datatypes/pigsCantFly.hs
@@ -0,0 +1,17 @@
+module PigsCantFly where
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = if x > 0 then x else - x
+
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y = ((snd x, snd y), (fst x, fst y))
+
+x = (+)
+f' xs = w `x` 1
+  where w = length xs
+
+id = \x -> x
